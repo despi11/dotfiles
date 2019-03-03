c0a327f64293c54d344424d128c17427f7c084cb
Maros Kramar <maroskramar2@gmail.com>
2019-03-03 23:49:33 +0100

    f
    a
    A
    A
    A

---
+++ fish/config.fish
@@ -0,0 +1,38 @@
+#!/usr/bin/fish
+
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_untrackedfiles '☡'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
+
+function fish_prompt
+	set_color yellow --bold
+	printf "%s" "$USER"
+	set_color green
+	printf "@"
+	set_color blue
+	printf "%s" "$hostname"
+	set_color magenta
+	printf "%s" "$CWD"
+
+
+	#git
+	set last_status $status
+
+	set_color red
+	printf '%s' (prompt_pwd)
+	set_color normal
+
+	printf '%s ' (__fish_git_prompt)
+
+	set_color normal
+end
